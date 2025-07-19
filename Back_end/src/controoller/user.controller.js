a4b461ebbf7e70989fb4cfd5265d9a757b004606
Subroto112001 <subrotokumarbarman@gmail.com>
2025-07-19 10:52:48 +0600

    Added Back_end as normal folder (fixed submodule issue)

---
+++ Back_end/src/controoller/user.controller.js
@@ -0,0 +1,109 @@
+const usermodal = require('../models/user.model')
+
+exports.registration = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { userName, email, avatar, phoneNumber, password } = req.body;
+
+    if (!userName) {
+      return res.status(401).json({
+        msg: "Username Missing",
+      });
+    }
+    if (!email) {
+      return res.status(401).json({
+        msg: "email Missing",
+      });
+    }
+    if (!avatar) {
+      return res.status(401).json({
+        msg: "avatar Missing",
+      });
+    }
+    if (!phoneNumber) {
+      return res.status(401).json({
+        msg: "phoneNumber Missing",
+      });
+    }
+    if (!password) {
+      return res.status(401).json({
+        msg: "password Missing",
+      });
+    }
+
+    /**
+     * todo : is there any email already exist we will find it
+     *
+     * */
+
+    const isExist = await usermodal.findOne({ email: email });
+    console.log(isExist);
+    if (isExist) {
+      return res.status(401).json({
+        msg: `${email} is already exist in databse, Try another one`,
+      });
+    }
+   
+
+    /**
+     * todo : here now we will save the data in databse
+     * */
+
+    usermodal.create({
+      userName,
+      email,
+      avatar,
+      phoneNumber,
+      password,
+      ...req.body,
+    });
+    return res.status(200).json({
+      msg: "Registration Succesfull",
+    });
+  } catch (error) {
+      console.log("error from user registration controller", error);
+      res.status(501).json({
+        msg: " Server error",
+        error: error,
+      });
+    
+  }
+}
+
+exports.login = async (req, res) => {
+    try {
+
+        // if (!email) {
+        //   return res.status(401).json({
+        //     msg: "email Missing",
+        //   });
+        // }
+       
+        // if (!password) {
+        //   return res.status(401).json({
+        //     msg: "password Missing",
+        //   });
+        // }
+        
+
+
+console.log(req.body);
+
+        const isExist = await usermodal.findOne({
+          $and: [{ email: req.body.email, password: req.body.password }],
+        });
+        console.log(isExist);
+     
+        if (!isExist) {
+          return res.status(401).json({
+            msg: `${email} Email and password in invalid`,
+          });
+        }
+    return res.status(200).json({
+      msg: "login Succesfull",
+    });
+    } catch (error) {
+        console.log("Error from login", error);
+        
+    }
+}
---
+++ Back_end/src/models/category.modal.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const categoryShema = new Schema(
+  {
+    categoryName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    categoryDescription: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("category", categoryShema);
